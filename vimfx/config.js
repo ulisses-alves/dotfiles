0229714e14d1d4e9d0e9c009c3552d75b5bcd6ed
Ulisses Bini de Paiva Alves <ulissesbpalves@gmail.com>
2016-08-14 11:13:22 -0300

    add vimfx config files

---
+++ vimfx/config.js
@@ -0,0 +1,13 @@
+var up = 'l'
+var down = 'k'
+var left = 'j'
+var right = ';'
+
+vimfx.set('mode.normal.scroll_up', up)
+vimfx.set('mode.normal.scroll_down', down)
+vimfx.set('mode.normal.scroll_left', left)
+vimfx.set('mode.normal.scroll_right', right)
+vimfx.set('mode.caret.move_up', up)
+vimfx.set('mode.caret.move_down', down)
+vimfx.set('mode.caret.move_left', left)
+vimfx.set('mode.caret.move_right', right)
